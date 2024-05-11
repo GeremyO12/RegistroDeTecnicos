2f2bd228f994268a8048ae4d281ebfe7520d9aa6
Geremy De los Santos S. <geremiorlando28@hotmail.com>
2024-05-11 15:45:09 -0400

    Crecion de Tecnicos

---
+++ RegistroDeTecnicos/Models/Tecnicos.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroDeTecnicos.Models
+{
+	public class Tecnicos
+	{
+		[Key]
+		public int TecnicoId { get; set; }
+		[Required(ErrorMessage = "El campo descripci&oacute;n es obligatorio")]
+		public string Descripcion { get; set; }
+	}
+}
