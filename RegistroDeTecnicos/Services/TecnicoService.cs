d902f3081f436879436a406c18c1ede55a436b36
Geremy De los Santos S. <geremiorlando28@hotmail.com>
2024-05-11 16:59:42 -0400

    Creacion de TecnicoService

---
+++ RegistroDeTecnicos/Services/TecnicoService.cs
@@ -0,0 +1,15 @@
+﻿using RegistroDeTecnicos.DAL;
+using RegistroDeTecnicos.Models;
+using System.Linq.Expressions;
+
+namespace RegistroDeTecnicos.Services
+{
+	public class TecnicoService
+	{
+		private readonly Contexto _contexto;
+		public TecnicoService(Contexto contexto)
+		{
+			_contexto = contexto;
+		}
+	}
+}
