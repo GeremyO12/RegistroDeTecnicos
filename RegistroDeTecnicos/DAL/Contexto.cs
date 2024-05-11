fb94e9e0064f35474c85424b64f68842c85609fd
Geremy De los Santos S. <geremiorlando28@hotmail.com>
2024-05-11 16:02:22 -0400

    Creacion de Contexto

---
+++ RegistroDeTecnicos/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroDeTecnicos.Models;
+
+namespace RegistroDeTecnicos.DAL
+{
+	public class Contexto : DbContext
+	{
+		public Contexto(DbContextOptions<Contexto> options)
+		     : base(options){ }
+		public DbSet<Tecnicos> Tecnicos { get; set; }
+	}
+}
